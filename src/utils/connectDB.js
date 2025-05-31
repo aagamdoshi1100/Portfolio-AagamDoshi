cde94e88e2cb313c47515531ab50c6764507bc21
aagamdoshi1100 <aagamdoshi1100@gmail.com>
2025-06-01 03:31:42 +0530

    fixed import error

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const MongoURI = process.env.Mongo_URI;
 
 const connectDB = async () => {
   try {
-    const response = await mongoose.connect(MongoURI, {});
+    await mongoose.connect(MongoURI, {});
     console.log("Connected to database", mongoose.connection.name);
   } catch (error) {
     console.log("Failed to connect database", error);
