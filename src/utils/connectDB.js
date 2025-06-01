7007198d65431ff9e202e38e6ca22ef700772eaa
aagamdoshi1100 <aagamdoshi1100@gmail.com>
2025-06-01 19:13:23 +0530

    Fixed Mongo URI issue

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-const MongoURI = process.env.Mongo_URI;
 
 const connectDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
   try {
-    await mongoose.connect(MongoURI, {});
+    await mongoose.connect(MONGODB_URI, {});
     console.log("Connected to database", mongoose.connection.name);
   } catch (error) {
     console.log("Failed to connect database", error);
